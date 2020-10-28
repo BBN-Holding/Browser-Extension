eced84ad45d5c3c0085bc195fa601f9f68daf206
Schlauer-Hax <arzberger52@gmail.com>
2020-10-28 20:47:03 +0100

    Added Teleparty Patch

--- background.js
+++ background.js
@@ -1,10 +1,25 @@
-chrome.browserAction.onClicked.addListener(function() {
+var netflixcode = `
+document.getElementById("title").children[0].innerText = "Netflix Party (BBN)"; 
+document.getElementById("teleparty-blog-btn").children[0].src = "https://www.bigbotnetwork.com/images/avatar.png";
+document.getElementById("teleparty-blog-btn").children[1].innerText = "Join Our Discord";
+document.getElementById("teleparty-blog-btn").children[1].style.background = "#7289DA";
+document.getElementById("teleparty-blog-btn").href = "https://discord.gg/nPwjaJk";
+console.log("[BBN Plugin] Made Teleparty Better");
+`
+
+chrome.browserAction.onClicked.addListener(function () {
     chrome.tabs.query({
         currentWindow: true,
         active: true
     }, function(tab) {
-        chrome.tabs.create({
-            "url": "https://bigbotnetwork.com"
-        });
-    });
+        if (tab[0].url.startsWith('https://www.netflix.com/watch/')) {
+            chrome.tabs.executeScript({
+                code: netflixcode
+            })
+        } else {
+            chrome.tabs.create({
+                "url": "https://bigbotnetwork.com"
+            });
+        }
+    })
 });
